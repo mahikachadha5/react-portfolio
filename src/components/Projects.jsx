06b64c843a4a61acb3d9aa983c012a11478ac7cb
mahikachadha5 <chadhama5@gmail.com>
2025-03-04 20:01:50 -0500

    updated projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,15 +7,12 @@ import { getImageURl } from '../utils';
 const projectsData = [
     { id: 1, name: "My Portfolio", description: "This website!", longDesc: "Completed in 12 hours, deployed using Netlify, and fully responsive across all devices!", 
         skills: ["React.js", "Javascript", "CSS Modules"], imageUrl: getImageURl("projects/portfolio.png"), sourceCode: "https://github.com/mahikachadha5/react-portfolio" }, 
-    { id: 2, name: "Spark", description: "Generate and track daily acts of kindness to spread positivity.", 
-        longDesc: "A web app designed to inspire and encourage acts of kindness in everyday life. The app generates daily or weekly suggestions for good deeds, offering users simple yet impactful ways to make a positive difference. Users can track their completed acts of kindness, earning points and climbing a leaderboard that fosters a sense of community and friendly competition. ~in progress~", 
-        skills: ["React", "Node.js", "Express", "Javascript"], imageUrl: getImageURl("projects/kindnessApp.png"),  },
-    { id: 3, name: "Recipe Finder", description: "Discover and organize recipes tailored to your preferences and dietary needs", 
+    { id: 2, name: "Recipe Finder", description: "Discover and organize recipes tailored to your preferences and dietary needs", 
         longDesc: "The app allows users to discover a wide variety of recipes tailored to their dietary preferences and needs. Users can save their favorite recipes and even create shopping lists based on selected ingredients. The app also offers features like user authentication, allowing for a secure and personalized cooking experience that evolves with the user's tastes and preferences.", 
         skills: ["Android Studio", "Java", "Firebase", "RESTful API"], imageUrl: getImageURl("projects/recipeAppGif.gif"), sourceCode: "https://github.com/neu-cs4520/final-project-groups4finalproject-4" },
-    { id: 4, name: "Travel Journal", description: "Travel journal of my trips while studying abroad.", longDesc: "This was my first solo React project, and a great way to compile my favorite memories and images from when I was abroad.", 
+    { id: 3, name: "Travel Journal", description: "Travel journal of my trips while studying abroad.", longDesc: "This was my first solo React project, and a great way to compile my favorite memories and images from when I was abroad.", 
         skills: ["React", "Javascript", "CSS"], imageUrl: getImageURl("projects/travelJournal.png"), sourceCode: "https://v2.scrimba.com/s005kmodh3" },
-    { id: 5, name: "Unplugged", description: "An app that helps you ~unplug~ from social media.", longDesc: "Unplugged is an app designed to help users discover engaging activities in their area, encouraging them to take a break from screens and connect with the world around them.", 
+    { id: 4, name: "Unplugged", description: "An app that helps you ~unplug~ from social media.", longDesc: "Unplugged is an app designed to help users discover engaging activities in their area, encouraging them to take a break from screens and connect with the world around them.", 
         skills: ["Figma"], imageUrl: getImageURl("projects/unplugged.png"), sourceCode: "https://www.figma.com/proto/H25mxEbFusN5oOlgVaOQI7/UNPLUGGED?node-id=226-1984&node-type=canvas&t=Z7aEin7uTfd3pfdX-0&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=85%3A1465&show-proto-sidebar=1" }, 
 ];
 
