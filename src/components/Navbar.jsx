271682bd5165077b4726a24a9b410b39762d2d04
mahikachadha5 <chadhama5@gmail.com>
2025-07-13 17:12:26 -0400

    cleanup navbar and add log

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,42 @@
-import {useState} from 'react';
+import { useState } from "react";
 
-import styles from "./modules/Navbar.module.css"
-import { getImageURl } from '../utils';
+import styles from "./modules/Navbar.module.css";
+import { getImageURl } from "../utils";
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    const [menuOpen, setMenuOpen] = useState(false);
+  return (
+    <nav className={styles.navbar}>
+      <img
+        href="/"
+        className={styles.title}
+        src={getImageURl("nav/Vector.png")}
+      />
 
-    return (
-        <nav className={`${styles.navbar} navbar`}>
-            <img href='/' className={styles.title} src={getImageURl('nav/Vector.png')}/>
-            
-            <div className={styles.menu}>
-                <img className={styles.menuBtn} 
-                src={menuOpen ? getImageURl("nav/closeIcon.png") : getImageURl("nav/menuIcon.png")} 
-                onClick={() => setMenuOpen(!menuOpen)}
-                alt='menu-button'
-                />
-                <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
-                onClick={() => setMenuOpen(false)}>
-                    <li><a href="#projects">PROJECTS</a></li>
-                    <li><a href="#contact">CONTACT</a></li>
-                </ul>
-            </div>
-        </nav>
-
-    )
+      <div className={styles.menu}>
+        <img
+          className={styles.menuBtn}
+          src={
+            menuOpen
+              ? getImageURl("nav/closeIcon.png")
+              : getImageURl("nav/menuIcon.png")
+          }
+          onClick={() => setMenuOpen(!menuOpen)}
+          alt="menu-button"
+        />
+        <ul
+          className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+          onClick={() => setMenuOpen(false)}
+        >
+          <li>
+            <a href="#projects">PROJECTS</a>
+          </li>
+          <li>
+            <a href="#contact">CONTACT</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
