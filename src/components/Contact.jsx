5a0c750060eac9501a81ced8e344f68b9e80fd41
mahikachadha5 <chadhama5@gmail.com>
2025-07-13 17:08:43 -0400

    cleanup contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,34 +1,43 @@
-import React from 'react'
+import styles from "./modules/Contact.module.css";
+import { getImageURl } from "../utils";
 
-import styles from "./modules/Contact.module.css"
-import { getImageURl } from '../utils';
+const contacts = [
+  {
+    label: "chadhama5@gmail.com",
+    icon: "contact/emailIcon.png",
+    link: "mailto:chadhama5@gmail.com",
+    alt: "email icon",
+  },
+  {
+    label: "linkedin.com/mahikachadha",
+    icon: "contact/linkedinIcon.png",
+    link: "https://www.linkedin.com/in/mahika-chadha-43b299211/",
+    alt: "linkedin icon",
+  },
+  {
+    label: "github.com/mahikachadha",
+    icon: "contact/githubIcon.png",
+    link: "https://github.com/mahikachadha5",
+    alt: "github icon",
+  },
+];
 
 export default function Contact() {
-    return (
-        <footer id="contact" className={styles.contactSection}>
-            <div className={styles.text}>
-                <h2>Contact</h2>
-                <p>Feel free to reach out!</p>
-            </div>
-            <ul className={styles.links}>
-                <li className={styles.link}>
-                    <img src={getImageURl("contact/emailIcon.png")} alt="email icon" />
-                    <a href="mailto:chadha.ma@northeastern.edu">chadha.ma@northeastern.edu</a>
-                </li>
-                <li className={styles.link}>
-                    <img src={getImageURl("contact/linkedinIcon.png")} alt="linkedin icon" />
-                    <a href="https://www.linkedin.com/in/mahika-chadha-43b299211/">linkedin.com/mahikachadha</a>
-                </li>
-                <li className={styles.link}>
-                    <img src={getImageURl("contact/githubIcon.png")} alt="github icon" />
-                    <a href="https://github.com/mahikachadha5">github.com/mahikachadha</a>
-                </li>
-            </ul>
-            
-
-            
-
-        </footer>
-
-    )
+  return (
+    <footer id="contact" className={styles.contactSection}>
+      <div className={styles.text}>
+        <h2>CONTACT ME</h2>
+      </div>
+      <ul className={styles.links}>
+        {contacts.map(({ label, icon, link, alt }, index) => (
+          <li key={index} className={styles.link}>
+            <a href={link} target="_blank" rel="noopener noreferrer">
+              <img src={getImageURl(icon)} alt={alt} />
+              {label}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </footer>
+  );
 }
