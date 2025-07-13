819a88a4894b1feaf67b389b4812546d18a2f5d0
mahikachadha5 <chadhama5@gmail.com>
2025-07-13 17:13:19 -0400

    remove side panel and add hover animations

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,85 +1,66 @@
-import React, { useState } from 'react'
-
-import { getImageURl } from '../utils';
-
-
-
-const projectsData = [
-    { id: 1, name: "My Portfolio", description: "This website!", longDesc: "Completed in 12 hours, deployed using Netlify, and fully responsive across all devices!", 
-        skills: ["React.js", "Javascript", "CSS Modules"], imageUrl: getImageURl("projects/portfolio.png"), sourceCode: "https://github.com/mahikachadha5/react-portfolio" }, 
-    { id: 2, name: "Recipe Finder", description: "Discover and organize recipes tailored to your preferences and dietary needs", 
-        longDesc: "The app allows users to discover a wide variety of recipes tailored to their dietary preferences and needs. Users can save their favorite recipes and even create shopping lists based on selected ingredients. The app also offers features like user authentication, allowing for a secure and personalized cooking experience that evolves with the user's tastes and preferences.", 
-        skills: ["Android Studio", "Java", "Firebase", "RESTful API"], imageUrl: getImageURl("projects/recipeAppGif.gif"), sourceCode: "https://github.com/neu-cs4520/final-project-groups4finalproject-4" },
-    { id: 3, name: "Travel Journal", description: "Travel journal of my trips while studying abroad.", longDesc: "This was my first solo React project, and a great way to compile my favorite memories and images from when I was abroad.", 
-        skills: ["React", "Javascript", "CSS"], imageUrl: getImageURl("projects/travelJournal.png"), sourceCode: "https://v2.scrimba.com/s005kmodh3" },
-    { id: 4, name: "Unplugged", description: "An app that helps you ~unplug~ from social media.", longDesc: "Unplugged is an app designed to help users discover engaging activities in their area, encouraging them to take a break from screens and connect with the world around them.", 
-        skills: ["Figma"], imageUrl: getImageURl("projects/unplugged.png"), sourceCode: "https://www.figma.com/proto/H25mxEbFusN5oOlgVaOQI7/UNPLUGGED?node-id=226-1984&node-type=canvas&t=Z7aEin7uTfd3pfdX-0&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=85%3A1465&show-proto-sidebar=1" }, 
-];
-
-
-import styles from "./Projects.module.css"
+import { useState, useEffect } from "react";
+import { getProjects } from "../utils";
+import styles from "./modules/Projects.module.css";
+import global from "../App.module.css";
+import { Link } from "react-router-dom";
 
 export default function Projects() {
+  const [projects, setProjects] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    const [ selectedProject, setSelectedProject ] = useState(null);
-    const [ isSidePanelOpen, setIsSidePanelOpen ] = useState(null);
+  useEffect(() => {
+    const loadProjects = async () => {
+      try {
+        const projectsData = await getProjects();
+        setProjects(projectsData);
+        setLoading(false);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+    loadProjects();
+  }, []);
 
-    const handleProjectClick = (project) => {
-        setSelectedProject(project);
-        setIsSidePanelOpen(true);
-    }
+  return (
+    <div id="projects" className={styles.projectsContainer}>
+      <h2 className={global["heading-lg"]}>PROJECTS</h2>
+      {loading && <div className={styles.loading}>Loading projects...</div>}
 
-    const handleClosePanel = () => {
-        setIsSidePanelOpen(false);
-    }
+      {error && <div className={styles.error}>{error}</div>}
 
-
-    return (
-        <section id="projects" className={styles.projectsContainer}>
-            <h2 className={styles.projectTitle}>Projects</h2>
-            <p className={styles.comingSoon}>More coming soon!</p>
-            <div className={styles.imgContainer}>
-                {projectsData.map((project) => (
-                    <div key={project.id} className={styles.project} onClick={() => handleProjectClick(project)}>
-                        <img src={project.imageUrl} alt={project.name} className={styles.projectImage} />
-                        <div className={styles.projectOverlay}>
-                            <h1 className={styles.projectName}>{project.name}</h1>
-                            <p className={styles.projectDescription}>{project.description}</p>
-                            <div className={styles.skills}>
-                                {project.skills.map((skill, index) => (
-                                    <div key={index} className={styles.skillItem}>{skill}</div>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                ))}
+      {!loading && !error && (
+        <div className={styles.imgContainer}>
+          {projects.map((project) => (
+            <div key={project.id} className={styles.project}>
+              <Link
+                className={styles.projectLink}
+                to={`/projects/${project.id}`}
+              >
+                <img
+                  src={project.imageUrl}
+                  alt={project.name}
+                  className={styles.projectImage}
+                />
+                <div className={styles.projectOverlay}>
+                  <h1 className={styles.projectName}>{project.name}</h1>
+                  <p className={styles.projectDescription}>
+                    {project.description}
+                  </p>
+                  <div className={styles.skills}>
+                    {project.skills.map((skill, index) => (
+                      <div key={index} className={styles.skillItem}>
+                        {skill}
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </Link>
             </div>
-
-            {/* side panel */}
-            <div className={`${styles.sidePanel} ${isSidePanelOpen ? styles.open : ''}`}>
-                <img src="/assets/projects/backBtn.png" onClick={handleClosePanel} className={styles.closeBtn} />
-                <hr className={styles.hr}/>
-                {selectedProject && (
-                    <div className={styles.sidePanelContent}>
-                        <h2 className={styles.sidePanelTitle}>{selectedProject.name}</h2>
-                        <img className={styles.sidePanelImg}src={selectedProject.imageUrl} alt="project image" />
-                        <h2 className={styles.subheadings}>About</h2>
-                        <p className={styles.sidePanelDesc}>{selectedProject.longDesc}</p>
-                        <h2 className={styles.subheadings}>Technologies</h2>
-                        <div className={styles.sidePanelSkills}>
-                                {selectedProject.skills.map((skill, index) => (
-                                    <div key={index} className={styles.sidePanelSkillItem}>{skill}</div>
-                                ))}
-                            </div>
-                        <h2 className={styles.subheadings}>Take a Look</h2>
-                        <a href={selectedProject.sourceCode} className={styles.sidePanelLink}>Click here to check it out!</a>
-                        
-                    </div>
-                )}
-            </div>
-        </section>
-
-    )
-
-
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
