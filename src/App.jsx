96314ca6171bfedd147031737789d7eb6d8105fc
mahikachadha5 <chadhama5@gmail.com>
2025-07-13 17:08:03 -0400

    add route for project detail component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
-
-import styles from './App.module.css'
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-
+import styles from "./App.module.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import CursorBlob from "./components/CursorBlob";
+import ProjectDetail from "./components/ProjectDetail";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className={styles.App}>
-    <Navbar />
-    <Hero />
-    <Projects />
-    <Contact />
-  </div>
+  return (
+    <div className={styles.App}>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <CursorBlob />
+              <Navbar />
+              <Hero />
+              <Projects />
+              <Contact />
+            </>
+          }
+        />
+        <Route path="/projects/:id" element={<ProjectDetail />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
